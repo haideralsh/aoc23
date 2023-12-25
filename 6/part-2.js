2e018c19cb5a31e6935b2f678fbb2f7c5efb4832
Haider Alshamma <haidera@nulogy.com>
2023-12-24 19:03:22 -0500

    Solve day 6, part 2

---
+++ 6/part-2.js
@@ -0,0 +1,20 @@
+import { input } from "./input.js"
+
+let [time, distance] = input.split("\n").map(line => Number(line.match(/\d+/g).join("")))
+
+let marginError = 1
+let newRecords = 0
+let record = distance
+
+for (let speed = 1; speed < time - 1; speed++) {
+	let time_left = time - speed
+	let distance = time_left * speed
+
+	if (distance > record) {
+		newRecords++
+	}
+}
+
+marginError *= newRecords
+
+console.log(marginError)
