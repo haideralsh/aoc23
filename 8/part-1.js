3a93f7cd5a2dd955aa1db4b6bd784b930cfbfa9f
Haider Alshamma <haidera@nulogy.com>
2023-12-26 21:33:19 -0500

    Solve day 8

---
+++ 8/part-1.js
@@ -0,0 +1,32 @@
+import { input } from "./input"
+
+let [instructions, groups] = input.split("\n\n")
+
+groups = groups.split("\n")
+				.map(group => group.match(/([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)/))
+				.reduce((result, reg) => ({ ...result, [reg[1]]: [reg[2], reg[3]] }), {})
+
+let target = "AAA"
+let steps = 0
+let index = 0
+
+while (target !== "ZZZ") {
+	if (index === instructions.length) index = 0
+
+	let [left, right] = groups[target]
+
+	switch (instructions[index]) {
+		case "R":
+			target = right
+			break
+
+		case "L":
+			target = left
+			break
+	}
+
+	index++
+	steps++
+}
+
+console.log(steps)
